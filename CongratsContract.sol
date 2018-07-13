e691956ec166fab8ad84aeaedb1b95ea4e8ad190
Alex Kalina <kalinalex91@gmail.com>
2018-07-13 13:09:57 +0300

    Move to new smart contract

--- CongratsContract.sol
+++ CongratsContract.sol
@@ -1,13 +1,11 @@
 pragma solidity ^0.4.24;
-import "github.com/Arachnid/solidity-stringutils/strings.sol";
-
 
 // Roma, s Dnem Rozhdenia!
 contract RomasCongrats {
-    using strings for *;
     address owner;
     string congrats;
     string picture;
+    bool isLocked = false;
 
     constructor() public {
         owner = msg.sender;
@@ -20,15 +18,28 @@ contract RomasCongrats {
         _;
     }
 
+    modifier isNotLocked() {
+        require(isLocked == false);
+        _;
+    }
+
+    function lock() public onlyOwner {
+        isLocked = true;
+    }
+
+    function checkIsLocked() public constant returns(bool) {
+        return isLocked;
+    }
+
     function transferOwnership(address newOwner) public onlyOwner {
 	    owner = newOwner;
     }
 
-    function setASCIIPicture(string newPic) public onlyOwner {
+    function setASCIIPicture(string newPic) public onlyOwner isNotLocked {
         picture = newPic;
     }
 
-    function setCongrats(string newCongrats) public onlyOwner {
+    function setCongrats(string newCongrats) public onlyOwner isNotLocked {
         congrats = newCongrats;
     }
 
